7bb92ce1b5c18083489037599a942a64f48a2661
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-04-11 14:10:42 +0900

    SNS認証を行うす為のメソッドを実装

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -21,6 +21,26 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super
   # end
 
+  def google_oauth2
+    authorization
+  end
+
+  def failure
+    redirect_to root_path
+  end
+
+  private
+
+  def authorization
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      render template: 'devise/registrations/new'
+    end
+  end
+
   # protected
 
   # The path used when OmniAuth fails
