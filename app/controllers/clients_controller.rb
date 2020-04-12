890a9130d6857c01fdaa3e2c2e4b6e48e0ca907c
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-04-12 16:56:53 +0900

    clientコントローラー（new, editアクション）の設定

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,8 +3,21 @@ class ClientsController < ApplicationController
   end
 
   def new
+    @client = Client.new
   end
 
   def create
+    @client = Client.new(client_params)
+    if @client.save
+      redirect_to root_path, notice: 'クライアントを登録しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:company_name, :responsible_party, :address, :postal_code, :phone_number, :email, :user_id)
   end
 end
