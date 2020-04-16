bf09e9ddcc6405d44d4f3e64170257f3ff959abc
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-04-16 17:29:07 +0900

    quotation, minutes, otherモデルのカラム修正

--- db/migrate/20200414080749_create_quotations.rb
+++ db/migrate/20200414080749_create_quotations.rb
@@ -2,12 +2,11 @@ class CreateQuotations < ActiveRecord::Migration[5.2]
   def change
     create_table :quotations do |t|
       t.string :name
-      t.string :file,       null: false
       t.integer :row_order, null: false
       t.references :client, null: false, foreign_key: true
       t.timestamps
     end
 
-    add_index :quotations, [:name, :file]
+    add_index :quotations, :name
   end
 end
--- db/migrate/20200414081849_create_others.rb
+++ db/migrate/20200414081849_create_others.rb
@@ -2,12 +2,11 @@ class CreateOthers < ActiveRecord::Migration[5.2]
   def change
     create_table :others do |t|
       t.string :name
-      t.string :file,       null: false
       t.integer :row_order, null: false
       t.references :client, null: false, foreign_key: true
       t.timestamps
     end
 
-    add_index :others, [:name, :file]
+    add_index :others, :name
   end
 end
