4a7836b0b862f2c31ffb5996c09d9f0beaa0ebe9
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-04-18 13:59:43 +0900

    deviseをrspecで使用できるようにする為の設定

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe UsersController do
+
+  describe 'GET #edit' do
+
+  #   it "edit.html.hamlに遷移すること" do
+  #     user = create(:user)
+  #     get :edit, params: { nickname: user, email: user, id: user }
+  #     expect(response).to render_template :edit
+  #   end
+  # end
+
+  # describe 'POST #create' do
+  #   it "saves the new contact in the database" do
+  #     expect{
+  #       post :create, article: attributes_for(:article)
+  #     }.to change(Article, :count).by(1)
+  #   end
+  #   it "redirects to articles#index" do
+  #     post :create, article: attributes_for(:article)
+  #     expect(response).to redirect_to articles_path
+  #   end
+  # end
+
+end
