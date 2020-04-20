06538b797c4d39a2078664c13ba55778de8c4286
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-04-20 11:00:16 +0900

    minuteモデルのバリデーションテスト

--- spec/models/minute_spec.rb
+++ spec/models/minute_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Minute, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#create' do
+
+    it "nameとemail、row_order、user_idが存在すれば登録できること" do
+      minute = create(:minute)
+      expect(minute).to be_valid
+    end
+
+    it "row_orderが存在しなくても登録できること" do
+      minute = create(:minute, row_order: nil)
+      expect(minute).to be_valid
+    end
+
+    it "nameがない場合は登録できないこと" do
+      minute = build(:minute, name: nil)
+      minute.valid?
+      expect(minute.errors[:name]).to include("を入力してください")
+    end
+
+  end
 end
