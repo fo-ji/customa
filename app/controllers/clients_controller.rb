0db25aaac1e58843d52ccb187e491661a9f33a9c
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-05-16 15:21:29 +0900

    [WIP]fileの並び替え機能の実装

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,9 +4,9 @@ class ClientsController < ApplicationController
 
   def index
     @clients    = Client.where(user_id: current_user.id)
-    @quotations = Document.where(user_id: current_user.id, category: 0)
-    @minutes    = Document.where(user_id: current_user.id, category: 1)
-    @others     = Document.where(user_id: current_user.id, category: 2)
+    @quotations = Document.where(user_id: current_user.id, category: 0).rank(:row_order)
+    @minutes    = Document.where(user_id: current_user.id, category: 1).rank(:row_order)
+    @others     = Document.where(user_id: current_user.id, category: 2).rank(:row_order)
   end
   
   def selected_client
@@ -19,6 +19,12 @@ class ClientsController < ApplicationController
         @selected_others     = selected_client.documents.where(category: 2)
       end
     end
+  end
+
+  def sort
+    document = Document.find(params[:document_id])
+    document.update(document_params)
+    render nothing: true
   end
 
   def new
@@ -65,5 +71,9 @@ class ClientsController < ApplicationController
 
   def client_params
     params.require(:client).permit(:company_name, :responsible_party, :address, :postal_code, :phone_number, :email).merge(user_id: current_user.id)
+  end
+
+  def document_params
+    params.require(:document).permit(:row_order_position)
   end
 end
